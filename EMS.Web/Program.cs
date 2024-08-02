cfcc5beccfb01f524d4d3289a72e206497da95d3
Kali <bashaferoz027@gmail.com>
2024-08-02 20:10:31 +0530

    DbService

--- EMS.Web/Program.cs
+++ EMS.Web/Program.cs
@@ -1,7 +1,12 @@
+using EMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
