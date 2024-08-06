e6554e7566ae5a66aae6e83294b4cb2724fdbb0b
Feroze basha S <bashaferoz66@gmail.com>
2024-08-06 10:54:52 +0530

    Db

--- EMS.Infrastructure/Repositories/EmployeeRepository.cs
+++ EMS.Infrastructure/Repositories/EmployeeRepository.cs
@@ -5,6 +5,15 @@ namespace EMS.Infrastructure.Repositories;
 
 public class EmployeeRepository(ApplicationDbContext context) :GenericRepository<Employee>(context),IEmployeeRepository
 {
+    public override async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        return await context.Employees.Include(e => e.PersonalDetails)
+            .Include(e => e.Qualifications)
+            .Include(e => e.Experiences)
+            .Include(e => e.GovernmentDocuments)
+            .Include(e => e.Certifications).ToListAsync();
+    }
+
     public override async Task<Employee> GetByIdAsync(Guid id)
     {
         return await context.Employees
--- EMS.Infrastructure/Repositories/Interfaces/IGenericRepository.cs
+++ EMS.Infrastructure/Repositories/Interfaces/IGenericRepository.cs
@@ -2,7 +2,7 @@ namespace EMS.Infrastructure.Repositories.Interfaces;
 
 public interface IGenericRepository<T> where T:class
 {
-    Task<IEnumerable<T>> GetAllAsync();
+    public Task<IEnumerable<T>> GetAllAsync();
     Task<T> GetByIdAsync(Guid id);
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
