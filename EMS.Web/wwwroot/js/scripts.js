03c74cd27e1932a519da72937d00144139d4ec96
Kali <bashaferoz027@gmail.com>
2024-08-07 23:03:27 +0530

    change

--- EMS.Web/wwwroot/js/scripts.js
+++ EMS.Web/wwwroot/js/scripts.js
@@ -1,29 +1,62 @@
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
+//
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (sidebarToggle) {
+//         //Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+// // Toggle the right side navigation
+//     const rightSidebarToggle = document.body.querySelector('#rightSidebarToggle');
+//     if (rightSidebarToggle) {
+//         // Uncomment Below to persist right sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|right-sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-right-sidenav-toggled');
+//         // }
+//         rightSidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-right-sidenav-toggled');
+//             localStorage.setItem('sb|right-sidebar-toggle', document.body.classList.contains('sb-right-sidenav-toggled'));
+//         });
+//     }
+// });
 
+document.addEventListener('DOMContentLoaded', () => {
     // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const sidebarToggle = document.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        //Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
-    }
-// Toggle the right side navigation
-    const rightSidebarToggle = document.body.querySelector('#rightSidebarToggle');
-    if (rightSidebarToggle) {
-        // Uncomment Below to persist right sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|right-sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-right-sidenav-toggled');
+
+        // Uncomment below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
+    }
+
+    // Toggle the right side navigation
+    const rightSidebarToggle = document.querySelector('#rightSidebarToggle');
+    if (rightSidebarToggle) {
         rightSidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-right-sidenav-toggled');
             localStorage.setItem('sb|right-sidebar-toggle', document.body.classList.contains('sb-right-sidenav-toggled'));
         });
+
+        // Uncomment below to persist right sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|right-sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-right-sidenav-toggled');
+        // }
     }
 });
+
